3bd72033a85afe92d98b9c6c0c7d9088713b3b19
RalXYZ <RalXYZ@pm.me>
2021-11-05 14:24:54 +0800

    chore: configure maven project

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main extends JFrame {
+    Main() {
+        this.setTitle("MiniCAD");
+        this.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Main();
+    }
+}
